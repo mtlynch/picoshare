18390ddf957999fb4977f307d2d19efc6fa9a398
Michael Lynch <git@mtlynch.io>
2022-02-15 21:25:39 -0500

    Fix e2e test

--- e2e/cypress/integration/auth_spec.js
+++ e2e/cypress/integration/auth_spec.js
@@ -1,8 +1,16 @@
 it("logs in and logs out", () => {
   cy.visit("/");
-  cy.get('.navbar-item[data-test-id="navbar-log-in"]').click();
+  cy.get('.navbar-item[data-test-id="log-in"]').click();
 
   cy.location("pathname").should("eq", "/login");
   cy.get('form input[type="password"]').type("dummypass");
   cy.get("form").submit();
+
+  cy.location("pathname").should("eq", "/");
+  // Using force here because I can't figure out how to get Cypress to expose
+  // the dropdown menu.
+  cy.get("#navbar-log-out").click({ force: true });
+
+  cy.location("pathname").should("eq", "/");
+  cy.get('.navbar-item[data-test-id="log-in"]').should("be.visible");
 });
