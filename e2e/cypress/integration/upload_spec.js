81d757f9fb9b4399b945ba287df6089db4b5bf1e
Michael Lynch <git@mtlynch.io>
2022-02-17 21:56:56 -0500

    Add e2e test for upload

---
+++ e2e/cypress/integration/upload_spec.js
@@ -0,0 +1,12 @@
+it("uploads a file and views it", () => {
+  cy.visit("/");
+  cy.get('.navbar-item .button[data-test-id="log-in"]').click();
+
+  cy.location("pathname").should("eq", "/login");
+  cy.get('form input[type="password"]').type("dummypass");
+  cy.get("form").submit();
+
+  cy.location("pathname").should("eq", "/");
+
+  cy.get(".file-input").attachFile("kittyface.jpg");
+});
