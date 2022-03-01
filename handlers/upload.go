c731f83c6650f3e3a9cea841672283e29c6e4d80
Michael Lynch <git@mtlynch.io>
2022-02-28 20:56:04 -0500

    Move download handler to download.go

--- handlers/upload.go
+++ handlers/upload.go
@@ -10,9 +10,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/gorilla/mux"
 	"github.com/mtlynch/picoshare/v2/random"
-	"github.com/mtlynch/picoshare/v2/store"
 	"github.com/mtlynch/picoshare/v2/types"
 )
 
@@ -27,33 +25,6 @@ var idCharacters = []rune("abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ2345
 
 type EntryPostResponse struct {
 	ID string `json:"id"`
-}
-
-func (s Server) entryGet() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		id, err := parseEntryID(mux.Vars(r)["id"])
-		if err != nil {
-			log.Printf("error parsing ID: %v", err)
-			http.Error(w, fmt.Sprintf("bad entry ID: %v", err), http.StatusBadRequest)
-			return
-		}
-
-		entry, err := s.store.GetEntry(id)
-		if _, ok := err.(store.EntryNotFoundError); ok {
-			http.Error(w, "entry not found", http.StatusNotFound)
-			return
-		} else if err != nil {
-			log.Printf("error retrieving entry with id %v: %v", id, err)
-			http.Error(w, "failed to retrieve entry", http.StatusInternalServerError)
-			return
-		}
-
-		if entry.Filename != "" {
-			w.Header().Set("Content-Disposition", fmt.Sprintf(`filename="%s"`, entry.Filename))
-		}
-
-		http.ServeContent(w, r, string(entry.Filename), entry.Uploaded, entry.Reader)
-	}
 }
 
 func (s Server) entryPost() http.HandlerFunc {
