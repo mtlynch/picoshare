9e6d7881fd5dcc79e9e0285c9e1a80726fe1eee6
Michael Lynch <git@mtlynch.io>
2022-03-06 09:10:22 -0500

    Removing console output

--- static/js/upload.js
+++ static/js/upload.js
@@ -76,21 +76,15 @@ document
 const uploadForm = document.getElementById("upload-form");
 uploadForm.addEventListener("drop", (evt) => {
   evt.preventDefault();
-  console.log(evt);
   if (evt.dataTransfer.items) {
-    console.log("accessing as items");
-    // Use DataTransferItemList interface to access the file(s)
     for (var i = 0; i < evt.dataTransfer.items.length; i++) {
-      // If dropped items aren't files, reject them
       if (evt.dataTransfer.items[i].kind === "file") {
         var file = evt.dataTransfer.items[i].getAsFile();
-        console.log("... file[" + i + "].name = " + file.name);
         doUpload(file, expirationSelect.value);
+        return;
       }
     }
   } else {
-    console.log("accessing as files");
-    // Use DataTransfer interface to access the file(s)
     for (var i = 0; i < evt.dataTransfer.files.length; i++) {
       console.log(
         "... file[" + i + "].name = " + evt.dataTransfer.files[i].name
@@ -98,9 +92,11 @@ uploadForm.addEventListener("drop", (evt) => {
     }
   }
 });
+
 uploadForm.addEventListener("dragover", (evt) => {
   evt.preventDefault();
 });
+
 uploadForm.addEventListener("dragenter", (evt) => {
   evt.preventDefault();
 });
