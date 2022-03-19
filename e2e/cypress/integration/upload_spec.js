05cbce8b2960608e0924fe9fbdf082b7f427b823
Michael Lynch <git@mtlynch.io>
2022-03-19 11:13:06 -0400

    Fix e2e tests

--- e2e/cypress/integration/upload_spec.js
+++ e2e/cypress/integration/upload_spec.js
@@ -9,10 +9,20 @@ it("uploads a file", () => {
   cy.location("pathname").should("eq", "/");
 
   cy.get(".file-input").attachFile("kittyface.jpg");
-  cy.get("#upload-result upload-links").should("be.visible");
+
+  cy.get("#upload-result .message-body").should("contain", "Upload complete!");
+
   cy.get("#upload-result upload-links")
     .should("have.attr", "filename")
     .and("equal", "kittyface.jpg");
+  cy.get("#upload-result upload-links")
+    .shadow()
+    .find("#verbose-link")
+    .should("be.visible");
+  cy.get("#upload-result upload-links")
+    .shadow()
+    .find("#short-link")
+    .should("be.visible");
 
   cy.get('.navbar a[href="/files"]').click();
   cy.get('.table td[test-data-id="filename"]').should(
