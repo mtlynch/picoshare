9e45438d60b5b542cc2b30ecc01ee6526b2f9a6c
dertuerke <2199151+dertuerke@users.noreply.github.com>
2022-03-24 07:53:58 +0100

    Remove JS Filesize Calc

--- static/js/fileIndex.js
+++ static/js/fileIndex.js
@@ -17,18 +17,3 @@ document.querySelectorAll('[pico-purpose="delete"]').forEach((deleteBtn) => {
       });
   });
 });
-
-document.querySelectorAll(["[pico-file-size]"]).forEach((fileSize) => {
-  fileSize.innerText = formatBytes(fileSize.innerText);
-});
-
-function formatBytes(bytes, decimals = 2) {
-  if (bytes === 0) return "0 Bytes";
-
-  const k = 1024;
-  const dm = decimals < 0 ? 0 : decimals;
-  const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
-  const i = Math.floor(Math.log(bytes) / Math.log(k));
-
-  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
-}
