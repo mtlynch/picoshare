543f03f1b97cd376087f44b982f11a2e8479f83e
dertuerke <2199151+dertuerke@users.noreply.github.com>
2022-03-25 08:58:34 +0100

    format code

--- static/js/fileIndex.js
+++ static/js/fileIndex.js
@@ -1,42 +1,42 @@
-import { deleteFile } from './controllers/delete.js';
+import { deleteFile } from "./controllers/delete.js";
 
-const errorContainer = document.getElementById('error');
+const errorContainer = document.getElementById("error");
 
 function hideElement(el) {
-  el.classList.add('is-hidden');
+  el.classList.add("is-hidden");
 }
 
 function showElement(el) {
-  el.classList.remove('is-hidden');
+  el.classList.remove("is-hidden");
 }
 
 document.querySelectorAll('[pico-purpose="delete"]').forEach((deleteBtn) => {
-  deleteBtn.addEventListener('click', () => {
-    const id = deleteBtn.getAttribute('pico-entry-id');
+  deleteBtn.addEventListener("click", () => {
+    const id = deleteBtn.getAttribute("pico-entry-id");
     deleteFile(id)
       .then(() => {
         const rowEl = deleteBtn.parentElement.parentElement;
-        rowEl.classList.add('deleted-entry');
+        rowEl.classList.add("deleted-entry");
       })
       .catch((error) => {
-        document.getElementById('error-message').innerText = error;
+        document.getElementById("error-message").innerText = error;
         showElement(errorContainer);
       });
   });
 });
 
-document.querySelector('#error .delete').addEventListener('click', () => {
+document.querySelector("#error .delete").addEventListener("click", () => {
   hideElement(errorContainer);
 });
 
 document.querySelectorAll('[pico-purpose="copy"]').forEach((copyBtn) => {
-  copyBtn.addEventListener('click', () => {
-    const picoId = copyBtn.getAttribute('pico-entry-id');
+  copyBtn.addEventListener("click", () => {
+    const picoId = copyBtn.getAttribute("pico-entry-id");
     const shortLink = `${window.location.origin}/!${picoId}`;
 
     copyToClipboard(shortLink)
-      .then(() => console.log('text copied !'))
-      .catch(() => console.log('error'));
+      .then(() => console.log("text copied !"))
+      .catch(() => console.log("error"));
   });
 });
 
@@ -51,18 +51,18 @@ function copyToClipboard(textToCopy) {
     return navigator.clipboard.writeText(textToCopy);
   } else {
     // text area method
-    const textArea = document.createElement('textarea');
+    const textArea = document.createElement("textarea");
     textArea.value = textToCopy;
     // make the textarea out of viewport
-    textArea.style.position = 'fixed';
-    textArea.style.left = '-999999px';
-    textArea.style.top = '-999999px';
+    textArea.style.position = "fixed";
+    textArea.style.left = "-999999px";
+    textArea.style.top = "-999999px";
     document.body.appendChild(textArea);
     textArea.focus();
     textArea.select();
     return new Promise((res, rej) => {
       // here the magic happens
-      document.execCommand('copy') ? res() : rej();
+      document.execCommand("copy") ? res() : rej();
       textArea.remove();
     });
   }
