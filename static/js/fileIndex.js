65cc9398638133c1db40e7adaea76233d493de87
Michael Lynch <git@mtlynch.io>
2022-03-24 20:03:38 -0400

    Fix ESLint errors

--- static/js/fileIndex.js
+++ static/js/fileIndex.js
@@ -1,11 +1,17 @@
 import { deleteFile } from "./controllers/delete.js";
 
-const uploadEl = document.querySelector(".file");
-const resultEl = document.getElementById("upload-result");
 const errorContainer = document.getElementById("error");
 
+function hideElement(el) {
+  el.classList.add("is-hidden");
+}
+
+function showElement(el) {
+  el.classList.remove("is-hidden");
+}
+
 document.querySelectorAll('[pico-purpose="delete"]').forEach((deleteBtn) => {
-  deleteBtn.addEventListener("click", (evt) => {
+  deleteBtn.addEventListener("click", () => {
     const id = deleteBtn.getAttribute("pico-entry-id");
     deleteFile(id)
       .then(() => {
@@ -13,7 +19,12 @@ document.querySelectorAll('[pico-purpose="delete"]').forEach((deleteBtn) => {
         rowEl.classList.add("deleted-entry");
       })
       .catch((error) => {
-        console.error(error);
+        document.getElementById("error-message").innerText = error;
+        showElement(errorContainer);
       });
   });
+});
+
+document.querySelector("#error .delete").addEventListener("click", () => {
+  hideElement(errorContainer);
 });
