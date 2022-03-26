mtlynch/picoshare

commit c1ce1b30a5d29f340597debd87769f6071484ef5

Add shortlink to file table (#128)
* add shortlink input and copy button

* copy event

* format shortlink

* remove go function formatShortlink

* Add Copy to Clipboard Fn

* format code

* Add Snachbar

* Refactor Snackbar and Clipboard

static/js/fileIndex.js
@@ -1,4 +1,7 @@
 import { deleteFile } from "./controllers/delete.js";
+import { copyToClipboard } from "./lib/copyToClipboard.js";
+import { makeShortLink } from "./lib/links.js";
+import { createSnackbar } from "./lib/snackbar.js";
 
 const errorContainer = document.getElementById("error");
 
@@ -27,4 +30,18 @@ document.querySelectorAll('[pico-purpose="delete"]').forEach((deleteBtn) => {
 
 document.querySelector("#error .delete").addEventListener("click", () => {
   hideElement(errorContainer);
+});
+
+document.querySelectorAll('[pico-purpose="copy"]').forEach((copyBtn) => {
+  copyBtn.addEventListener("click", () => {
+    const picoId = copyBtn.getAttribute("pico-entry-id");
+    const shortLink = makeShortLink(picoId);
+
+    copyToClipboard(shortLink)
+      .then(() => createSnackbar("Link was copied!"))
+      .catch((error) => {
+        document.getElementById("error-message").innerText = error;
+        showElement(errorContainer);
+      });
+  });
 });

static/css/lib/snackbar.css
@@ -1,0 +1,44 @@
+#snackbar-container {
+  position: fixed;
+  bottom: 0px;
+  left: 50%;
+  transform: translate(-50%, 0);
+}
+
+.snackbar {
+  visibility: hidden;
+  background-color: #3488ce;
+  font-weight: 600;
+  margin-bottom: 0.5rem;
+  color: #fff;
+  text-align: center;
+  border-radius: 2px;
+  padding: 1rem;
+}
+
+.snackbar.show {
+  visibility: visible;
+  animation: fadein 0.5s, fadeout 0.5s 2.5s;
+}
+
+@keyframes fadein {
+  from {
+    bottom: 0;
+    opacity: 0;
+  }
+  to {
+    bottom: 30px;
+    opacity: 1;
+  }
+}
+
+@keyframes fadeout {
+  from {
+    bottom: 30px;
+    opacity: 1;
+  }
+  to {
+    bottom: 0;
+    opacity: 0;
+  }
+}

static/js/lib/copyToClipboard.js
@@ -1,0 +1,18 @@
+export function copyToClipboard(textToCopy) {
+  if (navigator.clipboard && window.isSecureContext) {
+    return navigator.clipboard.writeText(textToCopy);
+  } else {
+    const textArea = document.createElement("textarea");
+    textArea.value = textToCopy;
+    textArea.style.position = "fixed";
+    textArea.style.left = "-999999px";
+    textArea.style.top = "-999999px";
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+    return new Promise((res, rej) => {
+      document.execCommand("copy") ? res() : rej();
+      textArea.remove();
+    });
+  }
+}

static/js/lib/snackbar.js
@@ -1,0 +1,18 @@
+export function createSnackbar(message) {
+  if (document.querySelectorAll("#snackbar-container").length === 0) {
+    const snackbarContainer = document.createElement("div");
+    snackbarContainer.setAttribute("id", "snackbar-container");
+    document.body.appendChild(snackbarContainer);
+  }
+
+  const el = document.createElement("div");
+  el.classList.add("snackbar");
+  el.innerHTML = message;
+
+  document.getElementById("snackbar-container").append(el);
+  el.classList.add("show");
+
+  setTimeout(() => {
+    el.remove();
+  }, 3000);
+}

templates/file-index.html
@@ -1,5 +1,6 @@
 {{ define "style-tags" }}
 <link rel="stylesheet" type="text/css" href="/css/file-index.css" />
+<link rel="stylesheet" type="text/css" href="/css/lib/snackbar.css" />
 {{ end }}
 
 {{ define "script-tags" }}
@@ -20,7 +21,7 @@
         <th>Size</th>
         <th>Uploaded</th>
         <th>Expires</th>
-        <th></th>
+        <th class="has-text-right">Actions</th>
       </tr>
     </thead>
     <tbody>
@@ -31,9 +32,18 @@
         <td class="is-vcentered">{{ formatTime .Uploaded }}</td>
         <td class="is-vcentered">{{ formatExpiration .Expires }}</td>
         <td class="is-vcentered">
-          <button class="button is-danger" pico-purpose="delete" pico-entry-id="{{ .ID }}">
-            <i class="fa fa-trash" aria-hidden="true"></i>
-          </button>
+          <div class="field is-grouped is-justify-content-flex-end">
+            <p class="control">
+              <button class="button is-info" pico-purpose="copy" pico-entry-id="{{ .ID }}">
+                <i class="fa-solid fa-copy"></i>
+              </button>
+            </p>
+            <p class="control">
+              <button class="button is-danger" pico-purpose="delete" pico-entry-id="{{ .ID }}">
+                <i class="fa fa-trash" aria-hidden="true"></i>
+              </button>
+            </p>
+          </div>
         </td>
       </tr>
       {{ end }}
@@ -50,6 +60,7 @@
     <div id="error-message" class="message-body">Placeholder error.</div>
   </article>
 </div>
+
 {{ end }}
 
 {{ template "base.html" }}

commit 679a2bca1f9035dfa87d540941eafe3e42578e1d

Fix CSS for delete in file table
The logic for finding the row was flimsy and we accidentally broke it in c1ce1b30a5d29f340597debd87769f6071484ef5.

static/js/fileIndex.js
@@ -17,8 +17,15 @@ document.querySelectorAll('[pico-purpose="delete"]').forEach((deleteBtn) => {
     const id = deleteBtn.getAttribute("pico-entry-id");
     deleteFile(id)
       .then(() => {
-        const rowEl = deleteBtn.parentElement.parentElement;
-        rowEl.classList.add("deleted-entry");
+        let currentEl = deleteBtn.parentElement;
+        while (currentEl && currentEl.nodeName !== "TR") {
+          currentEl = currentEl.parentElement;
+        }
+        if (!currentEl) {
+          return;
+        }
+
+        currentEl.classList.add("deleted-entry");
       })
       .catch((error) => {
         document.getElementById("error-message").innerText = error;