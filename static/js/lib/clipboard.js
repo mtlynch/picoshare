a0000ad11eab89842aeb61b68ff1988f651ec83d
Michael Lynch <mtlynch@users.noreply.github.com>
2022-03-25 20:13:11 -0400

    Refactor clipboard.js (#143)

--- static/js/lib/clipboard.js
+++ static/js/lib/clipboard.js
@@ -1,18 +1,32 @@
 export function copyToClipboard(textToCopy) {
   if (navigator.clipboard && window.isSecureContext) {
-    return navigator.clipboard.writeText(textToCopy);
+    return writeToClipboard(textToCopy);
   } else {
-    const textArea = document.createElement("textarea");
-    textArea.value = textToCopy;
-    textArea.style.position = "fixed";
-    textArea.style.left = "-999999px";
-    textArea.style.top = "-999999px";
-    document.body.appendChild(textArea);
-    textArea.focus();
-    textArea.select();
-    return new Promise((res, rej) => {
-      document.execCommand("copy") ? res() : rej();
-      textArea.remove();
-    });
+    return legacyWriteToClipboard(textToCopy);
   }
+}
+
+// Copy text to clipboard using the modern clipboard API.
+function writeToClipboard(textToWrite) {
+  return navigator.clipboard.writeText(textToWrite);
+}
+
+// Copy text to clipboard using legacy APIs that work even if serving over a
+// non-HTTPS connection.
+function legacyWriteToClipboard(textToWriter) {
+  const textArea = document.createElement("textarea");
+  textArea.value = textToWriter;
+
+  // Position the textarea off-screen.
+  textArea.style.position = "fixed";
+  textArea.style.left = "-999999px";
+  textArea.style.top = "-999999px";
+  document.body.appendChild(textArea);
+
+  textArea.focus();
+  textArea.select();
+  return new Promise((resolve, reject) => {
+    document.execCommand("copy") ? resolve() : reject();
+    textArea.remove();
+  });
 }
