81f98ba704b475acd9b5b18e72579a99cd28ef80
Michael Lynch <mtlynch@users.noreply.github.com>
2022-03-27 15:50:41 -0400

    Improve organization for types (#154)

--- types/types.go
+++ types/types.go
@@ -5,24 +5,26 @@ import (
 	"time"
 )
 
-type EntryID string
-type Filename string
-type ContentType string
-type ExpirationTime time.Time
+type (
+	EntryID        string
+	Filename       string
+	ContentType    string
+	ExpirationTime time.Time
+
+	UploadMetadata struct {
+		ID          EntryID
+		Filename    Filename
+		ContentType ContentType
+		Uploaded    time.Time
+		Expires     ExpirationTime
+		Size        int
+	}
+
+	UploadEntry struct {
+		UploadMetadata
+		Reader io.ReadSeeker
+	}
+)
 
 // Treat a distant expiration time as sort of a sentinel value signifying a "never expire" option.
 var NeverExpire = ExpirationTime(time.Date(3000, time.January, 0, 0, 0, 0, 0, time.UTC))
-
-type UploadMetadata struct {
-	ID          EntryID
-	Filename    Filename
-	ContentType ContentType
-	Uploaded    time.Time
-	Expires     ExpirationTime
-	Size        int
-}
-
-type UploadEntry struct {
-	UploadMetadata
-	Reader io.ReadSeeker
-}
