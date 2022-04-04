a693ca25824304ec27f9f5cdfe0f95bc6141ae5b
Michael Lynch <mtlynch@users.noreply.github.com>
2022-04-03 22:00:50 -0400

    Add linter for SQL files (#179)
    * Add linter for SQL files
    
    * Run sqlfluff as root
    
    * Specify dialect and cut out CI noise

--- store/sqlite/migrations/001-create-entries-table.sql
+++ store/sqlite/migrations/001-create-entries-table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS entries (
-  id TEXT PRIMARY KEY,
-  filename TEXT,
-  content_type TEXT,
-  upload_time TEXT,
-  expiration_time TEXT
+    id TEXT PRIMARY KEY,
+    filename TEXT,
+    content_type TEXT,
+    upload_time TEXT,
+    expiration_time TEXT
 );
