7858f72fa026ea8f7f6bf4b77ff02f886e8160bd
Michael Lynch <git@mtlynch.io>
2022-04-17 17:24:25 -0400

    Improve structure of filename test

--- handlers/parse/filename_test.go
+++ handlers/parse/filename_test.go
@@ -1,6 +1,7 @@
 package parse_test
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 
@@ -10,49 +11,50 @@ import (
 
 func TestFilename(t *testing.T) {
 	for _, tt := range []struct {
-		description    string
-		filename       string
-		validExpected  bool
-		parsedExpected types.Filename
+		description string
+		input       string
+		output      types.Filename
+		valid       bool
 	}{
 		{
-			description:    "valid filename",
-			filename:       "dummy.png",
-			validExpected:  true,
-			parsedExpected: types.Filename("dummy.png"),
+			description: "valid filename",
+			input:       "dummy.png",
+			valid:       true,
+			output:      types.Filename("dummy.png"),
 		},
 		{
-			description:   "filename with backslashes",
-			filename:      `filename\with\backslashes.png`,
-			validExpected: false,
+			description: "filename with backslashes",
+			input:       `filename\with\backslashes.png`,
+			valid:       false,
 		},
 		{
-			description:   "filename that's just a dot",
-			filename:      ".",
-			validExpected: false,
+			description: "filename that's just a dot",
+			input:       ".",
+			valid:       false,
 		},
 		{
-			description:   "filename that's two dots",
-			filename:      "..",
-			validExpected: false,
+			description: "filename that's two dots",
+			input:       "..",
+			valid:       false,
 		},
 		{
-			description:   "filename that's five dots",
-			filename:      ".....",
-			validExpected: false,
+			description: "filename that's five dots",
+			input:       ".....",
+			valid:       false,
 		},
 		{
-			description:   "filename that's too long",
-			filename:      strings.Repeat("A", parse.MaxFilenameLen+1),
-			validExpected: false,
+			description: "filename that's too long",
+			input:       strings.Repeat("A", parse.MaxFilenameLen+1),
+			valid:       false,
 		},
 	} {
-		t.Run(tt.description, func(t *testing.T) {
-			parsedActual, errActual := parse.Filename(tt.filename)
-			if (errActual == nil) != tt.validExpected {
-				t.Errorf("%s: input [%s], got %v, want %v", tt.description, tt.filename, errActual, tt.validExpected)
-			} else if parsedActual != tt.parsedExpected {
-				t.Errorf("%s: input [%s], got %v, want %v", tt.description, tt.filename, parsedActual, tt.parsedExpected)
+		t.Run(fmt.Sprintf("%s [%s]", tt.description, tt.input), func(t *testing.T) {
+			filename, err := parse.Filename(tt.input)
+			if tt.valid && err != nil {
+				t.Fatalf("err=%v, want %v", err, !tt.valid)
+			}
+			if got, want := filename, tt.output; got != want {
+				t.Errorf("filename=%v, want %v", filename, want)
 			}
 		})
 	}
