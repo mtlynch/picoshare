649156c8860c235d9a442bd8f06b3e4603c6254a
Michael Lynch <git@mtlynch.io>
2022-04-23 10:20:21 -0400

    Rename enableCsp to enableContentSecurityPolicy

--- handlers/csp.go
+++ handlers/csp.go
@@ -2,7 +2,7 @@ package handlers
 
 import "net/http"
 
-func enableCsp(next http.Handler) http.Handler {
+func enforceContentSecurityPolicy(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Security-Policy", `default-src: "self"`)
 		next.ServeHTTP(w, r)
