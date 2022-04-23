cf6bea79458ca0bbb76d999d8b8860770f7c5b4a
Michael Lynch <mtlynch@users.noreply.github.com>
2022-04-23 10:32:02 -0400

    Fix CSP header syntax (#209)
    Make it actually work

--- handlers/csp.go
+++ handlers/csp.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func enforceContentSecurityPolicy(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Security-Policy", `default-src: "self"`)
+		w.Header().Set("Content-Security-Policy", "default-src 'self';")
 		next.ServeHTTP(w, r)
 	})
 }
