d1ec9f927064b408ad11f00b84534db7bfc754a7
Michael Lynch <git@mtlynch.io>
2022-04-23 11:22:05 -0400

    Fix CSP compatibility with custom elements

--- handlers/csp.go
+++ handlers/csp.go
@@ -1,10 +1,39 @@
 package handlers
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
 
 func enforceContentSecurityPolicy(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Security-Policy", "default-src 'self';")
+		directives := map[string][]string{
+			"default-src": {
+				"self",
+			},
+			"script-src": {
+				"self",
+				// HTML custom elements require unsafe-inline.
+				"unsafe-inline",
+			},
+			"style-src": {
+				"self",
+				// HTML custom elements require unsafe-inline.
+				"unsafe-inline",
+			},
+		}
+		policyParts := []string{}
+		for directive, keywords := range directives {
+			keywordsFormatted := make([]string, len(keywords))
+			for i, keyword := range keywords {
+				keywordsFormatted[i] = fmt.Sprintf("'%s'", keyword)
+			}
+			policyParts = append(policyParts, fmt.Sprintf("%s %s", directive, strings.Join(keywordsFormatted, " ")))
+		}
+		policy := strings.Join(policyParts, "; ")
+
+		w.Header().Set("Content-Security-Policy", policy)
 		next.ServeHTTP(w, r)
 	})
 }
