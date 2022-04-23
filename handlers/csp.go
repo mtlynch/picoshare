65486783156f9a03127d0be41a41bf4b291c534d
Michael Lynch <mtlynch@users.noreply.github.com>
2022-04-23 11:41:20 -0400

    Fix CSP compatibility with custom elements (#211)
    * Fix CSP compatibility with custom elements
    
    * Fix CSP formatting

--- handlers/csp.go
+++ handlers/csp.go
@@ -1,10 +1,50 @@
 package handlers
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
 
 func enforceContentSecurityPolicy(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Security-Policy", "default-src 'self';")
+		type cspDirective struct {
+			name   string
+			values []string
+		}
+		directives := []cspDirective{
+			{
+				name: "default-src",
+				values: []string{
+					"self",
+				},
+			},
+			{
+				name: "script-src",
+				values: []string{
+					"self",
+					"unsafe-inline",
+				},
+			},
+			{
+				name: "style-src",
+				values: []string{
+					"self",
+					"unsafe-inline",
+				},
+			},
+		}
+		policyParts := []string{}
+		for _, directive := range directives {
+			valuesFormatted := make([]string, len(directive.values))
+			for i, value := range directive.values {
+				valuesFormatted[i] = fmt.Sprintf("'%s'", value)
+			}
+			policyParts = append(policyParts, fmt.Sprintf("%s %s", directive.name, strings.Join(valuesFormatted, " ")))
+		}
+		policy := strings.Join(policyParts, "; ") + ";"
+
+		w.Header().Set("Content-Security-Policy", policy)
 		next.ServeHTTP(w, r)
 	})
 }
