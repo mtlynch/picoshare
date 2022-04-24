de66ca0d430ebcd26766c3272b94960c368e3598
Michael Lynch <mtlynch@users.noreply.github.com>
2022-04-24 11:31:42 -0400

    Merge file controllers into a single file (#219)
    * Merge file controllers into a single file
    
    * Reorder exported functions
    
    * Fix import

--- static/js/controllers/files.js
+++ static/js/controllers/files.js
@@ -65,3 +65,26 @@ export async function guestUploadFile(file, guestLinkID) {
       return Promise.reject(error);
     });
 }
+
+export async function deleteFile(id) {
+  return fetch(`/api/entry/${id}`, {
+    method: "DELETE",
+    credentials: "include",
+  })
+    .then((response) => {
+      if (!response.ok) {
+        return response.text().then((error) => {
+          return Promise.reject(error);
+        });
+      }
+      return Promise.resolve();
+    })
+    .catch((error) => {
+      if (error.message) {
+        return Promise.reject(
+          "Failed to communicate with server: " + error.message
+        );
+      }
+      return Promise.reject(error);
+    });
+}
