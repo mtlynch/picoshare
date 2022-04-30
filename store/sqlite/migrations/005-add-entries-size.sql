9b9f3e43bcc2cbd07a178be167b972f64d9b8f89
Michael Lynch <git@mtlynch.io>
2022-04-30 08:19:37 -0400

    Fix SQL formatting

--- store/sqlite/migrations/005-add-entries-size.sql
+++ store/sqlite/migrations/005-add-entries-size.sql
@@ -2,14 +2,11 @@ ALTER TABLE entries ADD COLUMN file_size INTEGER;
 
 -- Populate file sizes for legacy entries
 
-UPDATE
-  entries
-SET
-  file_size = (
-    SELECT
-        SUM(LENGTH(entries_data.chunk)) AS file_size
-      FROM
+UPDATE entries
+SET file_size = (
+    SELECT SUM(LENGTH(entries_data.chunk)) AS file_size
+    FROM
         entries_data
-      WHERE
+    WHERE
         entries.id = entries_data.id
-  );
+);
