258433ab8d8c82857b0efd971f6a22b56dabc556
Michael Lynch <git@mtlynch.io>
2022-04-30 08:07:10 -0400

    Add migration

--- store/sqlite/migrations/005-add-entries-size.sql
+++ store/sqlite/migrations/005-add-entries-size.sql
@@ -1,3 +1,14 @@
 ALTER TABLE entries ADD COLUMN file_size INTEGER;
 
--- TODO: populate the size column for existing entries
+-- Populate file sizes for legacy entries
+UPDATE
+  entries
+SET
+  file_size = (
+    SELECT
+        SUM(LENGTH(chunk)) AS file_size
+      FROM
+        entries_data
+      WHERE
+        entries.id = entries_data.id
+  );
