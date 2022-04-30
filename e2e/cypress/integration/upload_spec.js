89781585377412ed1ec45c01b2bfca52da815679
Michael Lynch <git@mtlynch.io>
2022-04-30 19:03:48 -0400

    Add stubs for e2e tests under new flow

--- e2e/cypress/integration/upload_spec.js
+++ e2e/cypress/integration/upload_spec.js
@@ -80,3 +80,121 @@ it("uploads a file with a note", () => {
     "For Pico, with Love and Squalor"
   );
 });
+
+it("uploads a file and deletes it", () => {
+  cy.visit("/");
+  cy.get('.navbar-item .button[data-test-id="log-in"]').click();
+
+  cy.location("pathname").should("eq", "/login");
+  cy.get('form input[type="password"]').type("dummypass");
+  cy.get("form").submit();
+
+  cy.location("pathname").should("eq", "/");
+
+  cy.get(".file-input").attachFile("kittyface.jpg");
+
+  cy.get("#upload-result .message-body").should("contain", "Upload complete!");
+
+  cy.get("#upload-result upload-links")
+    .should("have.attr", "filename")
+    .and("equal", "kittyface.jpg");
+  cy.get("#upload-result upload-links")
+    .shadow()
+    .find("#verbose-link-box")
+    .shadow()
+    .find("#link")
+    .should("be.visible");
+  cy.get("#upload-result upload-links")
+    .shadow()
+    .find("#short-link-box")
+    .shadow()
+    .find("#link")
+    .should("be.visible");
+
+  cy.get('.navbar a[href="/files"]').click();
+  cy.get('.table tbody tr:first-child [test-data-id="filename"]').should(
+    "contain",
+    "kittyface.jpg"
+  );
+  // TODO: Delete file
+});
+
+it("uploads a file and deletes its note", () => {
+  cy.visit("/");
+  cy.get('.navbar-item .button[data-test-id="log-in"]').click();
+
+  cy.location("pathname").should("eq", "/login");
+  cy.get('form input[type="password"]').type("dummypass");
+  cy.get("form").submit();
+
+  cy.location("pathname").should("eq", "/");
+
+  cy.get("#note").type("For Pico, with Love and Squalor");
+
+  cy.get(".file-input").attachFile("kittyface.jpg");
+
+  cy.get("#upload-result .message-body").should("contain", "Upload complete!");
+
+  cy.get("#upload-result upload-links")
+    .should("have.attr", "filename")
+    .and("equal", "kittyface.jpg");
+  cy.get("#upload-result upload-links")
+    .shadow()
+    .find("#verbose-link-box")
+    .shadow()
+    .find("#link")
+    .should("be.visible");
+  cy.get("#upload-result upload-links")
+    .shadow()
+    .find("#short-link-box")
+    .shadow()
+    .find("#link")
+    .should("be.visible");
+
+  cy.get('.navbar a[href="/files"]').click();
+  cy.get('.table tbody tr:first-child [test-data-id="filename"]').should(
+    "contain",
+    "kittyface.jpg"
+  );
+  // TODO: Delete note
+});
+
+it("uploads a file and edits its note", () => {
+  cy.visit("/");
+  cy.get('.navbar-item .button[data-test-id="log-in"]').click();
+
+  cy.location("pathname").should("eq", "/login");
+  cy.get('form input[type="password"]').type("dummypass");
+  cy.get("form").submit();
+
+  cy.location("pathname").should("eq", "/");
+
+  cy.get("#note").type("For Pico, with Love and Squalor");
+
+  cy.get(".file-input").attachFile("kittyface.jpg");
+
+  cy.get("#upload-result .message-body").should("contain", "Upload complete!");
+
+  cy.get("#upload-result upload-links")
+    .should("have.attr", "filename")
+    .and("equal", "kittyface.jpg");
+  cy.get("#upload-result upload-links")
+    .shadow()
+    .find("#verbose-link-box")
+    .shadow()
+    .find("#link")
+    .should("be.visible");
+  cy.get("#upload-result upload-links")
+    .shadow()
+    .find("#short-link-box")
+    .shadow()
+    .find("#link")
+    .should("be.visible");
+
+  cy.get('.navbar a[href="/files"]').click();
+  cy.get('.table tbody tr:first-child [test-data-id="filename"]').should(
+    "contain",
+    "kittyface.jpg"
+  );
+  // TODO: Edit note
+});
