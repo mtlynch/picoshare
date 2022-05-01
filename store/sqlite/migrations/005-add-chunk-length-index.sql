7cc109f9a0eb3741c7ec66cd8b6c8f2f48840235
Michael Lynch <git@mtlynch.io>
2022-05-01 07:35:28 -0400

    Silence sqlfluff error

--- store/sqlite/migrations/005-add-chunk-length-index.sql
+++ store/sqlite/migrations/005-add-chunk-length-index.sql
@@ -1,3 +1,4 @@
 -- Create an index for fast file size calculation.
 -- https://github.com/mtlynch/picoshare/issues/220
-CREATE INDEX length_index ON entries_data(id, LENGTH(chunk));
+CREATE INDEX li1
+ON entries_data(id, LENGTH(chunk));
