2fba10a2e4801a15183561d63f33f8fe52981eb3
Michael Lynch <mtlynch@users.noreply.github.com>
2022-05-01 21:59:48 -0400

    Move JS for file edit page to an external script (#241)

---
+++ static/js/fileEdit.js
@@ -0,0 +1,39 @@
+import { editFile } from "./controllers/files.js";
+import { showElement, hideElement } from "./lib/bulma.js";
+
+const editForm = document.getElementById("edit-form");
+const errorContainer = document.getElementById("error");
+const progressSpinner = document.getElementById("progress-spinner");
+
+function readFilename() {
+  return document.getElementById("filename").value || null;
+}
+
+function readNote() {
+  return document.getElementById("note").value || null;
+}
+
+document.getElementById("edit-form").addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  const id = document.getElementById("edit-form").getAttribute("data-entry-id");
+  if (!id) {
+    return;
+  }
+
+  hideElement(errorContainer);
+  hideElement(editForm);
+  showElement(progressSpinner);
+
+  editFile(id, readFilename(), readNote())
+    .then(() => {
+      document.location = "/files";
+    })
+    .catch((error) => {
+      document.getElementById("error-message").innerText = error;
+      showElement(errorContainer);
+      showElement(editForm);
+    })
+    .finally(() => {
+      hideElement(progressSpinner);
+    });
+});
