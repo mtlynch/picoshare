12b85b70066b6347e75e09e3bdac813a0d16f64b
Michael Lynch <mtlynch@users.noreply.github.com>
2022-06-04 11:04:33 -0400

    Allow JS console calls in dev mode (#269)

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,13 @@ module.exports = {
   },
   extends: ["plugin:cypress/recommended", "eslint:recommended"],
   rules: {
-    "no-console": "error",
+    // This will produce an error for console.log or console.warn in production
+    // and a warning in development console.error will not produce an error or
+    // warning https://eslint.org/docs/rules/no-console#options
+    "no-console": [
+      process.env.NODE_ENV === "production" ? "error" : "warn",
+      { allow: ["error"] },
+    ],
   },
   ignorePatterns: ["static/third-party/**/*.js"],
 };
