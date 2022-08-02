e4bdf4ba506599ed82ebf11e2943eca874b64556
Michael Lynch <git@mtlynch.io>
2022-08-02 08:06:09 -0400

    Ensure only one garbage collection runs at a time

--- garbagecollect/schedule.go
+++ garbagecollect/schedule.go
@@ -2,6 +2,7 @@ package garbagecollect
 
 import (
 	"log"
+	"sync"
 	"time"
 
 	"github.com/mtlynch/picoshare/v2/store"
@@ -10,6 +11,7 @@ import (
 type Scheduler struct {
 	collector Collector
 	ticker    *time.Ticker
+	mu        sync.Mutex
 }
 
 func NewScheduler(store store.Store, interval time.Duration) Scheduler {
@@ -19,14 +21,19 @@ func NewScheduler(store store.Store, interval time.Duration) Scheduler {
 	}
 }
 
-func (s Scheduler) StartAsync() {
+func (s *Scheduler) StartAsync() {
 	go func() {
 		for range s.ticker.C {
-			log.Printf("cleaning up expired entries")
-			err := s.collector.Collect()
-			if err != nil {
-				log.Printf("garbage collection failed: %v", err)
-			}
+			s.collect()
 		}
 	}()
+}
+
+func (s *Scheduler) collect() {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	log.Printf("cleaning up expired entries")
+	if err := s.collector.Collect(); err != nil {
+		log.Printf("garbage collection failed: %v", err)
+	}
 }
