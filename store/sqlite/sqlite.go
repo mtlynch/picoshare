b8fe06763e8e5e5a99bca1aec9837a6169a9ebd2
Michael Lynch <git@mtlynch.io>
2022-08-02 21:19:21 -0400

    Fix SQL indenting

--- store/sqlite/sqlite.go
+++ store/sqlite/sqlite.go
@@ -460,16 +460,16 @@ func (d db) Purge() error {
 	if _, err := d.ctx.Exec(`
 		DELETE FROM
 			entries_data
+		WHERE
+		id IN (
+			SELECT
+				DISTINCT entries_data.id AS entry_id
+			FROM
+				entries_data
+			LEFT JOIN
+				entries ON entries_data.id = entries.id
 			WHERE
-			id IN (
-				SELECT
-					DISTINCT entries_data.id AS entry_id
-				FROM
-					entries_data
-				LEFT JOIN
-					entries ON entries_data.id = entries.id
-				WHERE
-					entries.id IS NULL
+				entries.id IS NULL
 			)`); err != nil {
 		return err
 	}
