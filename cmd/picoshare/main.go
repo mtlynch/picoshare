ff5fb9f2eda2849e7d04bd326a7077b77d51fdae
Michael Lynch <git@mtlynch.io>
2022-08-06 13:49:55 -0400

    work in progress

--- cmd/picoshare/main.go
+++ cmd/picoshare/main.go
@@ -34,6 +34,8 @@ func main() {
 
 	store := sqlite.New(*dbPath)
 
+	log.Printf("vacuumDB=%v", *vacuumDb)
+
 	collector := garbagecollect.NewCollector(store, *vacuumDb)
 	gc := garbagecollect.NewScheduler(&collector, 7*time.Hour)
 	gc.StartAsync()
