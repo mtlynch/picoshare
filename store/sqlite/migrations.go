e21797819c469051291c0e804a811fd24bf81678
Michael Lynch <mtlynch@users.noreply.github.com>
2022-08-06 22:43:41 -0400

    Refactoring DB migrations logic (#311)

--- store/sqlite/migrations.go
+++ store/sqlite/migrations.go
@@ -1,7 +1,10 @@
 package sqlite
 
 import (
+	"context"
+	"database/sql"
 	"embed"
+	"fmt"
 	"log"
 	"path"
 	"sort"
@@ -15,6 +18,46 @@ type dbMigration struct {
 
 //go:embed migrations/*.sql
 var migrationsFs embed.FS
+
+func applyMigrations(ctx *sql.DB) {
+	var version int
+	if err := ctx.QueryRow(`PRAGMA user_version`).Scan(&version); err != nil {
+		log.Fatalf("failed to get user_version: %v", err)
+	}
+
+	migrations, err := loadMigrations()
+	if err != nil {
+		log.Fatalf("error loading database migrations: %v", err)
+	}
+
+	log.Printf("Migration counter: %d/%d", version, len(migrations))
+
+	for _, migration := range migrations {
+		if migration.version <= version {
+			continue
+		}
+		tx, err := ctx.BeginTx(context.Background(), nil)
+		if err != nil {
+			log.Fatalf("failed to create migration transaction %d: %v", migration.version, err)
+		}
+
+		_, err = tx.Exec(migration.query)
+		if err != nil {
+			log.Fatalf("failed to perform DB migration %d: %v", migration.version, err)
+		}
+
+		_, err = tx.Exec(fmt.Sprintf(`pragma user_version=%d`, migration.version))
+		if err != nil {
+			log.Fatalf("failed to update DB version to %d: %v", migration.version, err)
+		}
+
+		if err = tx.Commit(); err != nil {
+			log.Fatalf("failed to commit migration %d: %v", migration.version, err)
+		}
+
+		log.Printf("Migration counter: %d/%d", migration.version, len(migrations))
+	}
+}
 
 func loadMigrations() ([]dbMigration, error) {
 	migrations := []dbMigration{}
