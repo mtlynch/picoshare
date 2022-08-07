ba8b5e9cba6cccada9d40d513e1e2f14a5f3d70b
Michael Lynch <git@mtlynch.io>
2022-08-06 22:40:00 -0400

    Refactoring DB migrations logic

--- store/sqlite/sqlite.go
+++ store/sqlite/sqlite.go
@@ -3,7 +3,6 @@ package sqlite
 import (
 	"context"
 	"database/sql"
-	"fmt"
 	"io"
 	"log"
 	"time"
@@ -57,43 +56,7 @@ PRAGMA wal_autocheckpoint = 0;
 		log.Fatalf("failed to set pragmas: %v", err)
 	}
 
-	var version int
-	if err := ctx.QueryRow(`PRAGMA user_version`).Scan(&version); err != nil {
-		log.Fatalf("failed to get user_version: %v", err)
-	}
-
-	migrations, err := loadMigrations()
-	if err != nil {
-		log.Fatalf("error loading database migrations: %v", err)
-	}
-
-	log.Printf("Migration counter: %d/%d", version, len(migrations))
-
-	for _, migration := range migrations {
-		if migration.version <= version {
-			continue
-		}
-		tx, err := ctx.BeginTx(context.Background(), nil)
-		if err != nil {
-			log.Fatalf("failed to create migration transaction %d: %v", migration.version, err)
-		}
-
-		_, err = tx.Exec(migration.query)
-		if err != nil {
-			log.Fatalf("failed to perform DB migration %d: %v", migration.version, err)
-		}
-
-		_, err = tx.Exec(fmt.Sprintf(`pragma user_version=%d`, migration.version))
-		if err != nil {
-			log.Fatalf("failed to update DB version to %d: %v", migration.version, err)
-		}
-
-		if err = tx.Commit(); err != nil {
-			log.Fatalf("failed to commit migration %d: %v", migration.version, err)
-		}
-
-		log.Printf("Migration counter: %d/%d", migration.version, len(migrations))
-	}
+	applyMigrations(ctx)
 
 	return &db{
 		ctx:       ctx,
