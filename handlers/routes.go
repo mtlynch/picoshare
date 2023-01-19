2c7ed689f8caabde89f4a76d96331b4fac628ab6
Michael Lynch <mtlynch@users.noreply.github.com>
2023-01-19 07:39:11 -0500

    Replace \! in routes with - (#363)
    The ! is a bash special character, which messes things up when you paste PicoShare URLs in the terminal. It's easier to use dash, which isn't a special character for any major filesystem or shell.

--- handlers/routes.go
+++ handlers/routes.go
@@ -53,6 +53,10 @@ func (s *Server) routes() {
 	views.Use(upgradeToHttps)
 	views.Use(enforceContentSecurityPolicy)
 	views.HandleFunc("/login", s.authGet()).Methods(http.MethodGet)
+	views.PathPrefix("/-{id}").HandlerFunc(s.entryGet()).Methods(http.MethodGet)
+	views.PathPrefix("/-{id}/{filename}").HandlerFunc(s.entryGet()).Methods(http.MethodGet)
+	// Legacy routes for entries. We stopped using them because the ! has
+	// unintended side effects within the bash shell.
 	views.PathPrefix("/!{id}").HandlerFunc(s.entryGet()).Methods(http.MethodGet)
 	views.PathPrefix("/!{id}/{filename}").HandlerFunc(s.entryGet()).Methods(http.MethodGet)
 	views.PathPrefix("/g/{guestLinkID}").HandlerFunc(s.guestUploadGet()).Methods(http.MethodGet)
