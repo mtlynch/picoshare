519871cd72ba87005a1abea3052e42ece5e87ae8
Michael Lynch <git@mtlynch.io>
2023-01-22 02:55:45 -0500

    work in progress

--- picoshare/file_lifetime_test.go
+++ picoshare/file_lifetime_test.go
@@ -9,37 +9,31 @@ import (
 
 func TestFriendlyName(t *testing.T) {
 	for _, tt := range []struct {
-		description string
-		lifetime    picoshare.FileLifetime
-		expected    string
+		lifetime picoshare.FileLifetime
+		expected string
 	}{
 		{
-			description: "",
-			lifetime:    picoshare.NewFileLifetime(time.Hour * 24),
-			expected:    "1 day",
+			lifetime: picoshare.NewFileLifetime(time.Hour * 24),
+			expected: "1 day",
 		},
 		{
-			description: "",
-			lifetime:    picoshare.NewFileLifetime(time.Hour * 24 * 7),
-			expected:    "7 days",
+			lifetime: picoshare.NewFileLifetime(time.Hour * 24 * 7),
+			expected: "7 days",
 		},
 		{
-			description: "",
-			lifetime:    picoshare.NewFileLifetime(time.Hour * 24 * 30),
-			expected:    "30 days",
+			lifetime: picoshare.NewFileLifetime(time.Hour * 24 * 30),
+			expected: "30 days",
 		},
 		{
-			description: "",
-			lifetime:    picoshare.NewFileLifetime(time.Hour * 24 * 365),
-			expected:    "1 year",
+			lifetime: picoshare.NewFileLifetime(time.Hour * 24 * 365),
+			expected: "1 year",
 		},
 		{
-			description: "",
-			lifetime:    picoshare.NewFileLifetime(time.Hour * 24 * 365 * 10),
-			expected:    "10 years",
+			lifetime: picoshare.NewFileLifetime(time.Hour * 24 * 365 * 10),
+			expected: "10 years",
 		},
 	} {
-		t.Run(tt.description, func(t *testing.T) {
+		t.Run(tt.expected, func(t *testing.T) {
 			if got, want := tt.lifetime.FriendlyName(), tt.expected; got != want {
 				t.Errorf("friendlyName=%v, want=%v", got, want)
 			}
