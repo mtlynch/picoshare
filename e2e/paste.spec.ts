8ff1f8237fe24f013305822e1b4b4847522205b4
Michael Lynch <mtlynch@users.noreply.github.com>
2023-04-11 20:15:12 -0400

    Encode pasted text using UTF-8 (#424)
    Fixes #423
    
    Otherwise, if the user pastes UTF-8 text, it won't render correctly when
    it's viewed.

--- e2e/paste.spec.ts
+++ e2e/paste.spec.ts
@@ -52,3 +52,35 @@ test("pastes text in the upload input", async ({ page }) => {
 
   await expect(await page.innerText("body")).toEqual("I'm pasting dummy text!");
 });
+
+test("pastes text in the upload input that requires UTF-8", async ({
+  page,
+}) => {
+  await login(page);
+
+  await clipboardCopy(page, "璇疯緭鍏ユ偍鐨勯棶棰橈細");
+  await page.locator("#pastebox").focus();
+  await clipboardPaste(page);
+
+  await expect(page.locator("#upload-result .message-body")).toHaveText(
+    "Upload complete!"
+  );
+  await expect(page.locator("#upload-result upload-links")).toHaveAttribute(
+    "filename",
+    /pasted-.*/
+  );
+  await expect(
+    page.locator("#upload-result upload-links #verbose-link-box #link")
+  ).toBeVisible();
+  await expect(
+    page.locator("#upload-result upload-links #short-link-box #link")
+  ).toBeVisible();
+
+  await page
+    .locator("#upload-result upload-links #verbose-link-box #link")
+    .click();
+
+  await expect(await page.innerText("body")).toEqual(
+    "璇疯緭鍏ユ偍鐨勯棶棰橈細"
+  );
+});
