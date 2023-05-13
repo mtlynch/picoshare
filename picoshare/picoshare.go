4a56a0d96650fdca347e426fb2c6d7def4414f4a
Michael Lynch <mtlynch@users.noreply.github.com>
2023-05-13 12:02:05 -0400

    Infer content type when it wasn't specified at upload time (#430)
    Resolves #427 
    
    If the client didn't specify the content type for a file at upload time,
    we can try to infer it based on the filename. This is helpful for files
    like mp4s or mp3s that modern browsers can play natively instead of
    relying on the user to download it and find a client player.

--- picoshare/picoshare.go
+++ picoshare/picoshare.go
@@ -35,6 +35,10 @@ type (
 // Treat a distant expiration time as sort of a sentinel value signifying a "never expire" option.
 var NeverExpire = ExpirationTime(time.Date(2999, time.December, 31, 0, 0, 0, 0, time.UTC))
 
+func (f Filename) String() string {
+	return string(f)
+}
+
 func (et ExpirationTime) String() string {
 	return (time.Time(et)).String()
 }
