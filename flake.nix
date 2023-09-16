5e4aab4863c8c6a941571e6f09bc638b7be301d0
Michael Lynch <git@mtlynch.io>
2023-09-16 11:31:42 -0400

    Use node.js 20.6.1

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Dev environment for WhatGotDone";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -24,9 +24,9 @@
       }) {inherit system; };
 
       pkgs_for_nodejs = import (builtins.fetchTarball {
-        # 18.14.1 release
-        url = "https://github.com/NixOS/nixpkgs/archive/6adf48f53d819a7b6e15672817fa1e78e5f4e84f.tar.gz";
-        sha256 = "0p7m72ipxyya5nn2p8q6h8njk0qk0jhmf6sbfdiv4sh05mbndj4q";
+        # 20.6.1 release
+        url = "https://github.com/NixOS/nixpkgs/archive/78058d810644f5ed276804ce7ea9e82d92bee293.tar.gz";
+        sha256 = "1k0bsy98ybpb05ddlhj2w0xbzinnidl2cdl9ifq96lvi04xvns4d";
       }) {inherit system; };
     in
     {
@@ -35,7 +35,7 @@
           gopls
           gotools
           pkgs_for_go.go_1_19
-          pkgs_for_nodejs.nodejs-18_x
+          pkgs_for_nodejs.nodejs_20
           pkgs_for_sqlfluff.sqlfluff
         ];
 
