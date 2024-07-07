5e67bef0de16335b45ef58a8011aa43f0a9f52aa
Michael Lynch <git@mtlynch.io>
2024-07-07 10:30:34 -0400

    work in progress

--- flake.nix
+++ flake.nix
@@ -4,8 +4,8 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
 
-    # 1.21.1 release
-    go_dep.url = "github:NixOS/nixpkgs/78058d810644f5ed276804ce7ea9e82d92bee293";
+    # 1.22.3 release
+    go_dep.url = "github:NixOS/nixpkgs/a9858885e197f984d92d7fe64e9fff6b2e488d40";
 
     # 3.44.2 release
     sqlite_dep.url = "github:NixOS/nixpkgs/5ad9903c16126a7d949101687af0aa589b1d7d3d";
