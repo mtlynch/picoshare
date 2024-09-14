bae64b70204e05d2339c7601d01c990e06b5da96
Michael Lynch <git@mtlynch.io>
2024-09-14 10:55:34 -0400

    Add pragmas

--- main.go
+++ main.go
@@ -31,6 +31,15 @@ func main() {
 		log.Panic(err)
 	}
 
+	_, err = db.Exec(`
+			-- Apply Litestream recommendations: https://litestream.io/tips/
+			--PRAGMA busy_timeout = 5000; -- Crashes with no output
+			--PRAGMA synchronous = NORMAL; -- Crashes with no output
+			--PRAGMA wal_autocheckpoint = 0; -- Crashes with OOM log`)
+	if err != nil {
+		log.Panic(err)
+	}
+
 	for _, path := range os.Args[1:] {
 		func() {
 			file, err := os.Open(path)
