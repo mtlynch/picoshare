f383f7756758dee1ef8f1e785930b6cf1a86bf9e
Balkaneser <43721713+Balk-Z@users.noreply.github.com>
2024-12-06 20:27:48 +0100

    ensure existing databases are migrated properly

--- store/sqlite/migrations/008-migrate-to-guest-file-expiration.sql
+++ store/sqlite/migrations/008-migrate-to-guest-file-expiration.sql
@@ -3,3 +3,7 @@ RENAME COLUMN expiration_time to url_expiration_time;
 
 ALTER TABLE guest_links
 ADD file_expiration_time TEXT;
+
+UPDATE guest_links
+SET file_expiration_time = 'NEVER'
+WHERE file_expiration_time IS NULL;
