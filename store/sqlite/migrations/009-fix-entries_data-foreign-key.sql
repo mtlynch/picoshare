7e1ede841d387eb30492024facd2112711621560
Michael Lynch <git@mtlynch.io>
2025-01-01 10:45:36 -0500

    Remove logic for deleting orphaned rows

--- store/sqlite/migrations/009-fix-entries_data-foreign-key.sql
+++ store/sqlite/migrations/009-fix-entries_data-foreign-key.sql
@@ -23,3 +23,18 @@ ALTER TABLE new_entries_data RENAME TO entries_data;
 -- Recreate the index for file size calculation
 --CREATE INDEX idx_entries_data_length
 --ON entries_data (id, LENGTH(chunk));
+
+-- TODO: Explain this.
+DELETE FROM
+    entries_data
+WHERE
+    id
+    IN (
+        SELECT DISTINCT entries_data.id AS entry_id
+        FROM
+            entries_data
+        LEFT JOIN
+            entries ON entries_data.id = entries.id
+        WHERE
+            entries.id IS NULL
+    );
