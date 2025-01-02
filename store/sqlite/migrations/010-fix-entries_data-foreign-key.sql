03f1d5e582dd5b5d8e86c56003a510947ce62a25
Michael Lynch <git@mtlynch.io>
2025-01-01 20:04:11 -0500

    work in progress

--- store/sqlite/migrations/010-fix-entries_data-foreign-key.sql
+++ store/sqlite/migrations/010-fix-entries_data-foreign-key.sql
@@ -1,4 +1,7 @@
--- Create new table with correct foreign key
+-- In migration 003, we didn't update entries_data to point to the new entries
+-- table, so we have to fix it here. For some reason, the sqlite3 database
+-- driver we were using at the time (mattn/go-sqlite3) didn't notice the
+-- violation of the FOREIGN KEY constraint.
 CREATE TABLE new_entries_data (
     id TEXT,
     chunk_index INTEGER,
@@ -6,7 +9,6 @@ CREATE TABLE new_entries_data (
     FOREIGN KEY (id) REFERENCES entries (id)
 );
 
--- Copy all data from old table to new table
 INSERT INTO new_entries_data
 SELECT
     id,
@@ -14,27 +16,6 @@ SELECT
     chunk
 FROM entries_data;
 
--- Drop old table
 DROP TABLE entries_data;
 
--- Rename new table to original name
 ALTER TABLE new_entries_data RENAME TO entries_data;
-
--- Recreate the index for file size calculation
---CREATE INDEX idx_entries_data_length
---ON entries_data (id, LENGTH(chunk));
-
--- TODO: Explain this.
-DELETE FROM
-    entries_data
-WHERE
-    id
-    IN (
-        SELECT DISTINCT entries_data.id AS entry_id
-        FROM
-            entries_data
-        LEFT JOIN
-            entries ON entries_data.id = entries.id
-        WHERE
-            entries.id IS NULL
-    );
