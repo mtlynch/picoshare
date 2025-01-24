1864b9c294b20494c7f7d2ebbfd2b2bc6fca3539
Michael Lynch <git@mtlynch.io>
2025-01-24 12:33:15 -0500

    Add orphaned downloads migration

--- store/sqlite/migrations/012-delete-orphaned-download-history.sql
+++ store/sqlite/migrations/012-delete-orphaned-download-history.sql
@@ -1,1 +1,8 @@
--- TODO
+-- Delete download records that reference non-existent entries.
+-- Prior to #661, we were not delete download history when we deleted the
+-- associated file.
+DELETE FROM downloads
+WHERE entry_id NOT IN (
+    SELECT id
+    FROM entries
+);
