af334f05b784a8b872b1fe978746ebc8bbbcb6f8
Michael Lynch <mtlynch@users.noreply.github.com>
2025-01-24 16:05:32 -0500

    Fix entry_id data type in downloads table (#663)

---
+++ store/sqlite/migrations/011-downloads-id-as-text.sql
@@ -0,0 +1,22 @@
+-- In migration 007, we accidentally specified entry_id as an INTEGER instead of
+-- TEXT. This migration fixes the table so that entry_id has a correct TEXT
+-- type.
+CREATE TABLE new_downloads (
+    entry_id TEXT,
+    download_timestamp TEXT,
+    client_ip TEXT,
+    user_agent TEXT,
+    FOREIGN KEY (entry_id) REFERENCES entries (id)
+);
+
+INSERT INTO new_downloads
+SELECT
+    CAST(entry_id AS TEXT) AS entry_id,
+    download_timestamp,
+    client_ip,
+    user_agent
+FROM downloads;
+
+DROP TABLE downloads;
+
+ALTER TABLE new_downloads RENAME TO downloads;
