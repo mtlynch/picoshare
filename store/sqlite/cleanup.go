f0d009abf5e425d4710a41fec25ce134706e5f0e
Michael Lynch <git@mtlynch.io>
2025-01-24 12:06:14 -0500

    Ignore rollback error when tx is complete

--- store/sqlite/cleanup.go
+++ store/sqlite/cleanup.go
@@ -30,7 +30,7 @@ func (s Store) deleteExpiredEntries() error {
 	}
 
 	defer func() {
-		if err := tx.Rollback(); err != nil {
+		if err := tx.Rollback(); err != nil && err != sql.ErrTxDone {
 			log.Printf("failed to rollback delete expired entries: %v", err)
 		}
 	}()
