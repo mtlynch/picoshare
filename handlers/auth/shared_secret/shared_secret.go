0208eb7a960fb88369660866c70e9872411eba5c
Michael Lynch <git@mtlynch.io>
2025-06-05 19:14:33 -0400

    Rename auth to authenticator

--- handlers/auth/shared_secret/shared_secret.go
+++ handlers/auth/shared_secret/shared_secret.go
@@ -8,32 +8,32 @@ import (
 
 // SharedSecretAuthenticator handles authentication using a shared secret.
 type SharedSecretAuthenticator struct {
-	auth *httpAuth.Authenticator
+	authenticator *httpAuth.Authenticator
 }
 
 // New creates a new SharedSecretAuthenticator.
 func New(sharedSecretKey string) (SharedSecretAuthenticator, error) {
-	auth, err := httpAuth.New(sharedSecretKey)
+	authenticator, err := httpAuth.New(sharedSecretKey)
 	if err != nil {
 		return SharedSecretAuthenticator{}, err
 	}
 
 	return SharedSecretAuthenticator{
-		auth: auth,
+		authenticator: authenticator,
 	}, nil
 }
 
 // StartSession begins an authenticated session.
 func (ssa SharedSecretAuthenticator) StartSession(w http.ResponseWriter, r *http.Request) {
-	ssa.auth.StartSession(w, r)
+	ssa.authenticator.StartSession(w, r)
 }
 
 // Authenticate verifies if the request has valid authentication.
 func (ssa SharedSecretAuthenticator) Authenticate(r *http.Request) bool {
-	return ssa.auth.Authenticate(r)
+	return ssa.authenticator.Authenticate(r)
 }
 
 // ClearSession removes the authentication cookie.
 func (ssa SharedSecretAuthenticator) ClearSession(w http.ResponseWriter) {
-	ssa.auth.ClearSession(w)
+	ssa.authenticator.ClearSession(w)
 }
