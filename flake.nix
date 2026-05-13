1be936826db6334c5a5b942bea48e53071aaa705
Michael Lynch <mtlynch@users.noreply.github.com>
2026-05-13 17:07:05 -0400

    Upgrade to Playwright 1.59.1 (#763)

--- flake.nix
+++ flake.nix
@@ -22,8 +22,8 @@
     # 3.1.1 release
     sqlfluff-nixpkgs.url = "github:NixOS/nixpkgs/5629520edecb69630a3f4d17d3d33fc96c13f6fe";
 
-    # 1.57.0
-    playwright-nixpkgs.url = "github:NixOS/nixpkgs/5f02c91314c8ba4afe83b256b023756412218535";
+    # 1.59.1
+    playwright-nixpkgs.url = "github:NixOS/nixpkgs/7aaa00e7cc9be6c316cb5f6617bd740dd435c59d";
 
     # 0.1.131 release
     flyctl-nixpkgs.url = "github:NixOS/nixpkgs/09dc04054ba2ff1f861357d0e7e76d021b273cd7";
