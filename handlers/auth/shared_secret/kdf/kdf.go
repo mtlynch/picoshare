c14c63cc16d8f8cb244da512e78a1d31bb23a7f5
Michael Lynch <git@mtlynch.io>
2026-09-11 19:47:02 +0000

    Fix DeriveKey documentation

--- handlers/auth/shared_secret/kdf/kdf.go
+++ handlers/auth/shared_secret/kdf/kdf.go
@@ -18,8 +18,8 @@ var (
 	ErrInvalidSerialization = errors.New("invalid serialized key data")
 )
 
-// DeriveKeyFromSecret creates a derived key from the provided secret string
-// using PBKDF2 with hardcoded parameters.
+// DeriveKey creates a derived key from the provided passphrase using PBKDF2
+// with hardcoded parameters.
 func DeriveKey(passphrase picoshare.Passphrase) DerivedKey {
 	// These would be insecure values for storing a database of user credentials,
 	// but we're only storing a single password, so it's not important to have
