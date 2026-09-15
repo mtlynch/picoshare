596043a9464fcf2623fdbc4dd715f651625a9559
Michael Lynch <git@mtlynch.io>
2026-09-14 20:07:41 -0400

    Change back to master

---
+++ handlers/download_test.go
@@ -0,0 +1,174 @@
+package handlers_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/mtlynch/picoshare/handlers"
+	"github.com/mtlynch/picoshare/picoshare"
+	"github.com/mtlynch/picoshare/store/test_sqlite"
+)
+
+type mockEntry struct {
+	ID          picoshare.EntryID
+	Filename    picoshare.Filename
+	ContentType picoshare.ContentType
+}
+
+var (
+	dummyTextEntry = mockEntry{
+		ID:          "TTTTTTTTTT",
+		Filename:    picoshare.Filename("test.txt"),
+		ContentType: picoshare.ContentType("text/plain;charset=utf-8"),
+	}
+	dummyAudioEntry = mockEntry{
+		ID:          "AAAAAAAAAA",
+		Filename:    picoshare.Filename("test.mp3"),
+		ContentType: picoshare.ContentType("audio/mpeg"),
+	}
+	dummyAudioEntryWithoutContentType = mockEntry{
+		ID:       "AAAAAAAA22",
+		Filename: picoshare.Filename("test0.mp3"),
+	}
+	dummyVideoEntry = mockEntry{
+		ID:          "VVVVVVVVVV",
+		Filename:    picoshare.Filename("test.mp4"),
+		ContentType: picoshare.ContentType("video/mp4"),
+	}
+	dummyVideoEntryWithGenericContentType = mockEntry{
+		ID:          "VVVVVVVV22",
+		Filename:    picoshare.Filename("test0.mp4"),
+		ContentType: picoshare.ContentType("application/octet-stream"),
+	}
+	dummyHTMLEntry = mockEntry{
+		ID:          "HHHHHHHHHH",
+		Filename:    picoshare.Filename("payload.html"),
+		ContentType: picoshare.ContentType("text/html"),
+	}
+)
+
+func TestEntryGet(t *testing.T) {
+	for _, tt := range []struct {
+		description                string
+		requestRoute               string
+		expectedStatus             int
+		expectedContentDisposition string
+		expectedContentType        string
+		expectedCSP                string
+	}{
+		{
+			description:                "retrieves text entry",
+			requestRoute:               "/-TTTTTTTTTT",
+			expectedStatus:             http.StatusOK,
+			expectedContentDisposition: `filename="test.txt"`,
+			expectedContentType:        "text/plain;charset=utf-8",
+			expectedCSP:                "sandbox",
+		},
+		{
+			description:                "retrieves audio entry",
+			requestRoute:               "/-AAAAAAAAAA",
+			expectedStatus:             http.StatusOK,
+			expectedContentDisposition: `filename="test.mp3"`,
+			expectedContentType:        "audio/mpeg",
+			expectedCSP:                "sandbox",
+		},
+		{
+			description:                "retrieves audio entry and infers content-type when it wasn't specified at upload time",
+			requestRoute:               "/-AAAAAAAA22",
+			expectedStatus:             http.StatusOK,
+			expectedContentDisposition: `filename="test0.mp3"`,
+			expectedContentType:        "audio/mpeg",
+			expectedCSP:                "sandbox",
+		},
+		{
+			description:                "retrieves video entry",
+			requestRoute:               "/-VVVVVVVVVV",
+			expectedStatus:             http.StatusOK,
+			expectedContentDisposition: `filename="test.mp4"`,
+			expectedContentType:        "video/mp4",
+			expectedCSP:                "sandbox",
+		},
+		{
+			description:                "retrieves video entry and infers content-type when it wasn't specified at upload time",
+			requestRoute:               "/-VVVVVVVV22",
+			expectedStatus:             http.StatusOK,
+			expectedContentDisposition: `filename="test0.mp4"`,
+			expectedContentType:        "video/mp4",
+			expectedCSP:                "sandbox",
+		},
+		{
+			description:                "retrieves html entry",
+			requestRoute:               "/-HHHHHHHHHH",
+			expectedStatus:             http.StatusOK,
+			expectedContentDisposition: `filename="payload.html"`,
+			expectedContentType:        "text/html",
+			expectedCSP:                "sandbox",
+		},
+		{
+			description:    "request for non-existent entry returns 404",
+			requestRoute:   "/-ZZZZZZZZZZ",
+			expectedStatus: http.StatusNotFound,
+		},
+	} {
+		t.Run(tt.description, func(t *testing.T) {
+			dataStore := test_sqlite.New(t)
+
+			for _, mockEntry := range []mockEntry{
+				dummyTextEntry,
+				dummyAudioEntry,
+				dummyAudioEntryWithoutContentType,
+				dummyVideoEntry,
+				dummyVideoEntryWithGenericContentType,
+				dummyHTMLEntry,
+			} {
+				data := "dummy data"
+				entry := picoshare.UploadEntry{
+					UploadMetadata: picoshare.UploadMetadata{
+						ID:          mockEntry.ID,
+						Filename:    mockEntry.Filename,
+						ContentType: mockEntry.ContentType,
+						Uploaded:    mustParseTime("2023-01-01T00:00:00Z"),
+						Expires:     picoshare.NeverExpire,
+						Size:        mustParseFileSize(len(data)),
+					},
+					Reader: strings.NewReader(data),
+				}
+				if err := dataStore.InsertEntry(entry.Reader, entry.UploadMetadata); err != nil {
+					panic(err)
+				}
+			}
+
+			s := handlers.New(mockAuthenticator{}, &dataStore, nilSpaceCheckFunc, nilGarbageCollector, time.Now)
+
+			req := httptest.NewRequest(http.MethodGet, tt.requestRoute, nil)
+
+			rec := httptest.NewRecorder()
+			s.Router().ServeHTTP(rec, req)
+			res := rec.Result()
+
+			if got, want := res.StatusCode, tt.expectedStatus; got != want {
+				t.Fatalf("%s returned wrong status code: got %v want %v",
+					tt.requestRoute, got, want)
+			}
+
+			if tt.expectedStatus != http.StatusOK {
+				return
+			}
+
+			if got, want := res.Header.Get("Content-Disposition"), tt.expectedContentDisposition; got != want {
+				t.Errorf("Content-Disposition=%s, want=%s", got, want)
+			}
+
+			if got, want := res.Header.Get("Content-Type"), tt.expectedContentType; got != want {
+				t.Errorf("Content-Type=%s, want=%s", got, want)
+			}
+
+			if got, want := res.Header.Get("Content-Security-Policy"), tt.expectedCSP; got != want {
+				t.Errorf("Content-Security-Policy=%s, want=%s", got, want)
+			}
+		})
+	}
+}
